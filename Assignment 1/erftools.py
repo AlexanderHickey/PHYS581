e6fd8d8614a8fbd8109ccb2b996fde6a2d7d3001
Alex Hickey <alexhickey96@gmail.com>
2019-01-18 13:00:30 -0700

    Created erftools.py

---
+++ Assignment 1/erftools.py
@@ -0,0 +1,8 @@
+"""
+Author: Alexander Hickey
+
+This python library contains functions used to compute the complex
+error function. A testing framework is built in, and can be run 
+by loading this file into the __main__ namespace.
+"""
+
