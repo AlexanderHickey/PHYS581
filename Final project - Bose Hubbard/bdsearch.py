eef8ba602ec9e30b575b00ec3b944cbe12a3e5a7
Alex Hickey <alexhickey96@gmail.com>
2019-04-14 11:45:21 -0600

    update

---
+++ Final project - Bose Hubbard/bdsearch.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 14 11:36:39 2019
+
+@author: Alex Hickey
+
+Boundary search algorithm
+"""
+
---
+++ Final project - Bose Hubbard/variational.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 14 11:37:13 2019
+
+@author: Alex Hickey
+"""
+
